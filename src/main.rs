be25990765d1c691536c1149649783156b3d849f
Sofyan Sugianto <sofyanartem@gmail.com>
2022-11-28 14:00:52 +0700

    initialize bare-minimum gtk4 app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use gtk::prelude::*;
+use gtk::Application;
+
+const APP_ID: &str = "id.artemtech.gtk_rs.HelloWorld";
+
 fn main() {
-    println!("Hello, world!");
+    // Create app wrapper
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // run app
+    app.run();
 }
